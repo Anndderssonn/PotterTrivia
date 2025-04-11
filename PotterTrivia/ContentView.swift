83fa00870627ac4788b459e99a954f59ae72efae
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-04-10 19:48:09 -0500

    feat: Add animations and sound in the main screen

--- PotterTrivia/ContentView.swift
+++ PotterTrivia/ContentView.swift
@@ -6,8 +6,13 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var scalePlayButton = false
+    @State private var moveBkgImage = false
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
@@ -15,6 +20,12 @@ struct ContentView: View {
                     .resizable()
                     .frame(width: geometry.size.width * 3, height: geometry.size.height)
                     .padding(.top, 3)
+                    .offset(x: moveBkgImage ? geometry.size.width/1.1 : -geometry.size.width/1.1)
+                    .onAppear {
+                        withAnimation(.linear(duration: 60).repeatForever()) {
+                            moveBkgImage.toggle()
+                        }
+                    }
                 VStack {
                     VStack {
                         Image(systemName: "bolt.fill")
@@ -65,6 +76,12 @@ struct ContentView: View {
                                 .cornerRadius(5)
                                 .shadow(radius: 5)
                         }
+                        .scaleEffect(scalePlayButton ? 1.2 : 1)
+                        .onAppear {
+                            withAnimation(.easeInOut(duration: 1.3).repeatForever()) {
+                                scalePlayButton.toggle()
+                            }
+                        }
                         Spacer()
                         Button {
                             
@@ -83,6 +100,16 @@ struct ContentView: View {
             .frame(width: geometry.size.width, height: geometry.size.height)
         }
         .ignoresSafeArea()
+        .onAppear {
+//            playAudio()
+        }
+    }
+    
+    private func playAudio() {
+        let sound = Bundle.main.path(forResource: "magic-in-the-air", ofType: "mp3")
+        audioPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        audioPlayer.numberOfLoops = -1
+        audioPlayer.play()
     }
 }
 
