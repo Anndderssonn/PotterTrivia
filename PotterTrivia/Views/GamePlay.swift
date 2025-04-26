2651cbad943057e644a154457bc2435cf84a3852
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-04-26 15:44:31 -0500

    feat: Add reset function view and add GamePlay screen sounds

--- PotterTrivia/Views/GamePlay.swift
+++ PotterTrivia/Views/GamePlay.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct GamePlay: View {
     @Environment(\.dismiss) private var dismiss
     @Namespace private var namespace
+    @State private var musicPlayer: AVAudioPlayer!
+    @State private var sfxPlayer: AVAudioPlayer!
     @State private var animateViewsIn = false
     @State private var tappedCorrectAnswer = false
     @State private var hintWiggle = false
@@ -51,7 +54,7 @@ struct GamePlay: View {
                                 .opacity(tappedCorrectAnswer ? 0.1 : 1)
                         }
                     }
-                    .animation(.easeInOut(duration: 2), value: animateViewsIn)
+                    .animation(.easeInOut(duration: animateViewsIn ? 2 : 0), value: animateViewsIn)
                     Spacer()
                     HStack {
                         // MARK: Hints
@@ -75,6 +78,7 @@ struct GamePlay: View {
                                         withAnimation(.easeOut(duration: 1)) {
                                             revealHint = true
                                         }
+                                        playFlipSound()
                                     }
                                     .rotation3DEffect(.degrees(revealHint ? 1440 : 0), axis: (x: 0, y: 1, z: 0))
                                     .scaleEffect(revealHint ? 5 : 1)
@@ -92,7 +96,8 @@ struct GamePlay: View {
                                     .disabled(tappedCorrectAnswer)
                             }
                         }
-                        .animation(.easeOut(duration: 1.5).delay(2), value: animateViewsIn)
+                        .animation(.easeOut(duration: animateViewsIn
+                                            ? 1.5 : 0).delay(animateViewsIn ? 2 : 0), value: animateViewsIn)
                         Spacer()
                         VStack {
                             if animateViewsIn {
@@ -117,6 +122,7 @@ struct GamePlay: View {
                                         withAnimation(.easeOut(duration: 1)) {
                                             revealBook = true
                                         }
+                                        playFlipSound()
                                     }
                                     .rotation3DEffect(.degrees(revealBook ? 1440 : 0), axis: (x: 0, y: 1, z: 0))
                                     .scaleEffect(revealBook ? 5 : 1)
@@ -134,7 +140,7 @@ struct GamePlay: View {
                                     .disabled(tappedCorrectAnswer)
                             }
                         }
-                        .animation(.easeOut(duration: 1.5).delay(2), value: animateViewsIn)
+                        .animation(.easeOut(duration: animateViewsIn ? 1.5 : 0).delay(animateViewsIn ? 2 : 0), value: animateViewsIn)
                     }
                     .padding(.bottom)
                     LazyVGrid(columns: [GridItem(), GridItem()]) {
@@ -157,11 +163,12 @@ struct GamePlay: View {
                                                     withAnimation(.easeOut(duration: 1)) {
                                                         tappedCorrectAnswer = true
                                                     }
+                                                    playCorrectSound()
                                                 }
                                         }
                                     }
                                 }
-                                .animation(.easeOut(duration: 1).delay(1.5), value: animateViewsIn)
+                                .animation(.easeOut(duration: animateViewsIn ? 1 : 0).delay(animateViewsIn ? 1.5 : 0), value: animateViewsIn)
                             } else {
                                 VStack {
                                     if animateViewsIn {
@@ -177,13 +184,15 @@ struct GamePlay: View {
                                                 withAnimation(.easeOut(duration: 1)) {
                                                     wrongAnswerTapped.append(i)
                                                 }
+                                                playWrongSound()
+                                                giveWrongFeedback()
                                             }
                                             .scaleEffect(wrongAnswerTapped.contains(i) ? 0.8 : 1)
                                             .disabled(tappedCorrectAnswer || wrongAnswerTapped.contains(i))
                                             .opacity(tappedCorrectAnswer ? 0.1 : 1)
                                     }
                                 }
-                                .animation(.easeOut(duration: 1).delay(1.5), value: animateViewsIn)
+                                .animation(.easeOut(duration: animateViewsIn ? 1 : 0).delay(animateViewsIn ? 1.5 : 0), value: animateViewsIn)
                             }
                         }
                     }
@@ -218,7 +227,7 @@ struct GamePlay: View {
                                 .transition(.scale.combined(with: .offset(y: -geometry.size.height/2)))
                         }
                     }
-                    .animation(.easeInOut(duration: 1).delay(1), value: tappedCorrectAnswer)
+                    .animation(.easeInOut(duration: tappedCorrectAnswer ? 1 : 0).delay(tappedCorrectAnswer ? 1 : 0), value: tappedCorrectAnswer)
                     Spacer()
                     if tappedCorrectAnswer {
                         Text("Answer 1")
@@ -260,7 +269,7 @@ struct GamePlay: View {
                             }
                         }
                     }
-                    .animation(.easeInOut(duration: 2.7).delay(2.7), value: tappedCorrectAnswer)
+                    .animation(.easeInOut(duration: tappedCorrectAnswer ? 2.7 : 0).delay(tappedCorrectAnswer ? 2.7 : 0), value: tappedCorrectAnswer)
                     Group {
                         Spacer()
                         Spacer()
@@ -273,8 +282,41 @@ struct GamePlay: View {
         .ignoresSafeArea()
         .onAppear {
             animateViewsIn = true
-            tappedCorrectAnswer = false
+//            playMusic()
         }
+    }
+    
+    private func playMusic() {
+        let songs = ["let-the-mystery-unfold", "spellcraft", "hiding-place-in-the-forest", "deep-in-the-dell"]
+        let i = Int.random(in: 0..<songs.count)
+        let sound = Bundle.main.path(forResource: songs[i], ofType: "mp3")
+        musicPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        musicPlayer.volume = 0.1
+        musicPlayer.numberOfLoops = -1
+        musicPlayer.play()
+    }
+    
+    private func playFlipSound() {
+        let sound = Bundle.main.path(forResource: "page-flip", ofType: "mp3")
+        sfxPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        sfxPlayer.play()
+    }
+    
+    private func playWrongSound() {
+        let sound = Bundle.main.path(forResource: "negative-beeps", ofType: "mp3")
+        sfxPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        sfxPlayer.play()
+    }
+    
+    private func playCorrectSound() {
+        let sound = Bundle.main.path(forResource: "magic-wand", ofType: "mp3")
+        sfxPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        sfxPlayer.play()
+    }
+    
+    private func giveWrongFeedback() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.error)
     }
 }
 
